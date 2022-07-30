ac7fdbbb8c17c3dc63e559aa5e75e9fae5ceaff1
Maximilian Weigand <mweigand@mweigand.net>
2022-07-30 22:14:55 +0200

    tweak initrd generation script

--- initrd/gen_uboot_image.sh
+++ initrd/gen_uboot_image.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# generate an initrd image using dracut
+# note 1: remove the offscreen.bin include directive if you do not want to
+# 		  prepare an off-screen image.
+# note 2: the patch set from 2022.July.30 properly announces the required
+# firmware file. Older versions must include this parameter
+#   --install /lib/firmware/rockchip/ebc.wbf \
+
 # kernel modules
 version="5.17.0-rc6-next-20220304-g917a31bb1ac2-dirty"
 
@@ -10,7 +17,6 @@ test -e initrd.img && rm initrd.img
 	-o "lvm luks plymouth systemd resume" \
 	--add-drivers rockchip_ebc \
 	--omit-drivers "bluetooth hidp" \
-	--install /lib/firmware/rockchip/ebc.wbf \
 	--include ${PWD}/offscreen.bin /usr/lib/firmware/rockchip/rockchip_ebc_default_screen.bin \
 	dracut-initrd.img ${version}
 
